9a3586e83dad6a426a4b9ade14813acf6ce2db16
MrAbbas <de3ktop@gmail.com>
2025-07-04 16:07:29 +0330

    tunnelkit-installer.sh
    fix menu

--- tunnelkit-installer.sh
+++ tunnelkit-installer.sh
@@ -2,40 +2,93 @@
 #!/bin/bash
 
 # TunnelKit Installer Script
-# Author: parsdev (customized by ChatGPT)
-# GitHub: https://github.com/parsdev/tunnelkit
+# Author: parsdev
+# Version: 1.1
 
-clear
-echo -e "\e[1;36mTunnelKit Installation Menu\e[0m"
-echo "1) Install Backhaul"
-echo "2) Install Chisel"
-echo "3) Install FRP"
-echo "4) Install V2Ray (Sanaei Panel)"
-echo "0) Exit"
-read -p "Select an option: " opt
+while true; do
+  clear
+  echo -e "\e[1;36m🔧 TunnelKit Installation Menu\e[0m"
+  echo -e "\e[1;32m1)\e[0m Backhaul"
+  echo -e "\e[1;32m2)\e[0m Chisel"
+  echo -e "\e[1;32m3)\e[0m FRP"
+  echo -e "\e[1;32m4)\e[0m V2Ray (Sanaei Panel)"
+  echo -e "\e[1;31m0)\e[0m Exit"
+  echo ""
+  read -p "👉 Select an option: " opt
 
-case $opt in
-  1)
-    echo -e "\e[1;33mInstalling Backhaul...\e[0m"
-    # Installation steps placeholder
-    ;;
-  2)
-    echo -e "\e[1;33mInstalling Chisel...\e[0m"
-    # Installation steps placeholder
-    ;;
-  3)
-    echo -e "\e[1;33mInstalling FRP...\e[0m"
-    # Installation steps placeholder
-    ;;
-  4)
-    echo -e "\e[1;33mInstalling V2Ray Sanaei Panel...\e[0m"
-    # Installation steps placeholder
-    ;;
-  0)
-    echo -e "\e[1;31mExiting...\e[0m"
-    exit 0
-    ;;
-  *)
-    echo -e "\e[1;31mInvalid option. Try again.\e[0m"
-    ;;
-esac
+  case $opt in
+    1)
+      while true; do
+        clear
+        echo -e "\e[1;34m🔹 Backhaul Versions:\e[0m"
+        echo -e "\e[1;33m1)\e[0m Normal"
+        echo -e "\e[1;33m2)\e[0m Optimized"
+        echo -e "\e[1;33m3)\e[0m TCPMUX Version"
+        echo -e "\e[1;33m4)\e[0m Custom"
+        echo -e "\e[1;31m0)\e[0m Return to Main Menu"
+        read -p "🔍 Choose a version: " backhaul_opt
+        case $backhaul_opt in
+          1) echo -e "\e[1;36mInstalling Backhaul (Normal)...\e[0m"; sleep 2 ;;
+          2) echo -e "\e[1;36mInstalling Backhaul (Optimized)...\e[0m"; sleep 2 ;;
+          3) echo -e "\e[1;36mInstalling Backhaul (TCPMUX)...\e[0m"; sleep 2 ;;
+          4) echo -e "\e[1;36mInstalling Backhaul (Custom)...\e[0m"; sleep 2 ;;
+          0) break ;;
+          *) echo -e "\e[1;31mInvalid option. Try again.\e[0m"; sleep 1 ;;
+        esac
+        read -p "✔️ Press enter to return to Backhaul Menu..."
+      done
+      ;;
+    2)
+      while true; do
+        clear
+        echo -e "\e[1;34m🔹 Chisel Versions:\e[0m"
+        echo -e "\e[1;33m1)\e[0m Standard"
+        echo -e "\e[1;33m2)\e[0m Optimized"
+        echo -e "\e[1;33m3)\e[0m MUX Version"
+        echo -e "\e[1;33m4)\e[0m Custom TCP Handling"
+        echo -e "\e[1;31m0)\e[0m Return to Main Menu"
+        read -p "🔍 Choose a version: " chisel_opt
+        case $chisel_opt in
+          1) echo -e "\e[1;36mInstalling Chisel (Standard)...\e[0m"; sleep 2 ;;
+          2) echo -e "\e[1;36mInstalling Chisel (Optimized)...\e[0m"; sleep 2 ;;
+          3) echo -e "\e[1;36mInstalling Chisel (MUX)...\e[0m"; sleep 2 ;;
+          4) echo -e "\e[1;36mInstalling Chisel (Custom TCP)...\e[0m"; sleep 2 ;;
+          0) break ;;
+          *) echo -e "\e[1;31mInvalid option. Try again.\e[0m"; sleep 1 ;;
+        esac
+        read -p "✔️ Press enter to return to Chisel Menu..."
+      done
+      ;;
+    3)
+      while true; do
+        clear
+        echo -e "\e[1;34m🔹 FRP Installation:\e[0m"
+        echo -e "\e[1;33m1)\e[0m Setup FRP Client"
+        echo -e "\e[1;33m2)\e[0m Setup FRP Server"
+        echo -e "\e[1;31m0)\e[0m Return to Main Menu"
+        read -p "🔍 Choose a setup: " frp_opt
+        case $frp_opt in
+          1) echo -e "\e[1;36mInstalling FRP Client...\e[0m"; sleep 2 ;;
+          2) echo -e "\e[1;36mInstalling FRP Server...\e[0m"; sleep 2 ;;
+          0) break ;;
+          *) echo -e "\e[1;31mInvalid option. Try again.\e[0m"; sleep 1 ;;
+        esac
+        read -p "✔️ Press enter to return to FRP Menu..."
+      done
+      ;;
+    4)
+      echo -e "\e[1;36mInstalling V2Ray Sanaei Panel...\e[0m"
+      sleep 2
+      read -p "✔️ Press enter to return to main menu..."
+      ;;
+    0)
+      echo -e "\e[1;31m👋 Exiting TunnelKit... Goodbye!\e[0m"
+      break
+      ;;
+    *)
+      echo -e "\e[1;31m❌ Invalid option. Please try again.\e[0m"
+      sleep 2
+      ;;
+  esac
+done
+
